e16e34a9855e342556deb7da7ba1cb46ad60bf05
putri tip <it16@tiptech.network>
2018-09-26 10:51:49 +0700

    test

--- Slim/App.php
+++ Slim/App.php
@@ -6,7 +6,7 @@
  * @copyright Copyright (c) 2011-2017 Josh Lockhart
  * @license   https://github.com/slimphp/Slim/blob/3.x/LICENSE.md (MIT License)
  */
-namespace Slim;
+namespace Slim; 
 
 use Exception;
 use Slim\Exception\InvalidMethodException;
